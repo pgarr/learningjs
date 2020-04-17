2c1a02f1dc510e371d93117cb605fa3781e51fe8
pgarr <garlej.p@gmail.com>
2020-04-17 20:45:31 +0200

    intro-to-node

---
+++ Web Development Bootcamp/intro-to-node/index.js
@@ -0,0 +1,10 @@
+//jshint eversion:6
+
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var mySuperheroName = superheroes.random();
+var mySupervillainName = supervillains.random();
+
+console.log(mySuperheroName);
+console.log(mySupervillainName);
